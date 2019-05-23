959f9097e56f5b3e5e0b711d3668dc23ed0ecd4c
Annie Truex <artruex@gmail.com>
2019-05-23 17:15:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,8 +6,8 @@ def sort_array_desc(integers)
   integers.sort {|x,y| -(x <=> y)}
 end
 
-def sort_array_char_count(animals)
-  animals.sort do |a, b|
+def sort_array_char_count(string)
+  string.sort do |a, b|
     a <=> b 
 end
 end
