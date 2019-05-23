6710eb1eae4818baa414f85ecf40f45654d926ba
Annie Truex <artruex@gmail.com>
2019-05-23 17:19:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,12 +7,5 @@ def sort_array_desc(integers)
 end
 
 def sort_array_char_count(array)
-  array.sort do |a, b|
-    if a == b 
-      0 
-      elsif a < b 
-      -1
-      elsif a > b 
-      1 
-end
+ array.sort_by {|x| x.length}
 end
